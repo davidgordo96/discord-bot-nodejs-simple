a9538ae5f8f2b3aee6ff16498a5151332ae0cb01
davidgordo96 <davidgordoperez@gmail.com>
2021-06-09 20:10:54 +0200

    make it better

--- tokens.js
+++ tokens.js
@@ -1,2 +1,7 @@
-const token = 'ODUxNDkzMDk5NTk4MzgxMDg3.YL5Evg.7nZc0VxUQZXETb4U_RZ5cwtCCHo';
+const token = 'ODUxNDkzMDk5NTk4MzgxMDg3.YL5Evg.ZxV_SYaw3-ExM9jUmPqJaMZ9kac';
 const tokenRiot = 'RGAPI-1738dee4-bf09-4aee-b0df-2f2c2e99ee26';
+
+module.exports = {
+    "token" : token,
+    "tokenRiot" : tokenRiot
+}
