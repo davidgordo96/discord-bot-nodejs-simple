eb7c2179449a5471612bd0ae5f65ae70c0189a50
sergio <tuentigamer@gmail.com>
2021-10-09 19:45:06 +0200

    se saca el metodo reproductor de musica al archivo funciones

--- funciones.js
+++ funciones.js
@@ -35,8 +35,36 @@ let mmr= function (args,msg) {
     }
 }
 
+let musica = function(args,msg){
+    let voiceChannel = msg.member.voice.channel;
+    if (!voiceChannel || voiceChannel.type !== 'voice') {
+    msg.channel.send('¡Necesitas unirte a un canal de voz primero!.').catch(error => msg.channel.send(error));
+    } else if (msg.guild.voiceConnection) {
+    msg.channel.send('Ya estoy conectado en un canal de voz.');
+    } else {
+     msg.channel.send('Conectando...').then(m => {
+        voiceChannel.join().then(() => {
+               m.edit(':white_check_mark: | Conectado exitosamente.').catch(error => msg.channel.send(error));
+         }).catch(error => msg.channel.send(error));
+     }).catch(error => msg.channel.send(error));
+    }
+        // args[1] es la url de reproduccion 
+        const ytdl = require('ytdl-core');
+        if(!voiceChannel) return msg.channel.send('¡Necesitas unirte a un canal de voz primero!.');
+        if(args[1] == null || args[1] == undefined) return msg.channel.send('Ingrese un enlace de youtube para poder reproducirlo.');
+        voiceChannel.join()
+          .then(connection => {
+            const url = ytdl(args, { filter : 'audioonly' });
+            dispatcher = connection.play(url);
+            msg.channel.send('Reproduciendo ahora: '+ args);
+            msg.delete();
+          })
+          .catch(console.error);
+}
+
 
 
 module.exports = {
-    mmr
+    mmr,
+    musica
 }
