f7c82b41223f6b05762151448d3d3f3e0102c98d
davidgordo96 <35952082+davidgordo96@users.noreply.github.com>
2021-10-09 19:37:37 +0200

    Update tokens.js

--- tokens.js
+++ tokens.js
@@ -1,5 +1,5 @@
-const token = "ODUxNDkzMDk5NTk4MzgxMDg3.YL5Evg.B2Zdr-3pOaxjWQ0aanzanRPxJh0"
-const tokenRiot = "RGAPI-1cc30e4c-a726-4867-b77f-286b2b794ff3"
+const token = ""
+const tokenRiot = ""
 
 module.exports = {
     token : token,
